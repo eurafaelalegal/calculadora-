49a30fbf30b6377814cdfa9041fe7a591bf5366c
eurafaelalegal <167936144+eurafaelalegal@users.noreply.github.com>
2025-04-03 19:32:18 -0300

    calculadora

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+let display = document.getElementById('display');
+
+// Função para adicionar números e operadores no display
+function adicionar(valor) {
+  display.value += valor;
+}
+
+// Função para limpar o display
+function limpar() {
+  display.value = '';
+}
+
+// Função para calcular a expressão matemática
+function calcular() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = 'Erro';
+  }
+}
 
